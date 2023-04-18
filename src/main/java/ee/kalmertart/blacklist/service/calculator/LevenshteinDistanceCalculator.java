fb5f35dcc574deb2d76eb1c0b854b504ce0291a3
Kalmer Tart <kalmertart@gmail.com>
2023-04-18 15:30:32 +0300

    Refactoring to use self documenting variables

--- src/main/java/ee/kalmertart/blacklist/service/calculator/LevenshteinDistanceCalculator.java
+++ src/main/java/ee/kalmertart/blacklist/service/calculator/LevenshteinDistanceCalculator.java
@@ -6,23 +6,27 @@ import org.springframework.stereotype.Component;
 public class LevenshteinDistanceCalculator {
 
     public int calculate(String name, String blacklistedName) {
-        int[][] distance = new int[name.length() + 1][blacklistedName.length() + 1];
+        int[][] distances = new int[name.length() + 1][blacklistedName.length() + 1];
 
         for (int nameIndex = 0; nameIndex <= name.length(); nameIndex ++) {
             for (int blacklistedNameIndex = 0; blacklistedNameIndex <= blacklistedName.length(); blacklistedNameIndex ++) {
                 if (nameIndex == 0) {
-                    distance[nameIndex][blacklistedNameIndex] = blacklistedNameIndex;
+                    distances[nameIndex][blacklistedNameIndex] = blacklistedNameIndex;
                 } else if (blacklistedNameIndex == 0) {
-                    distance[nameIndex][blacklistedNameIndex] = nameIndex;
+                    distances[nameIndex][blacklistedNameIndex] = nameIndex;
                 } else {
+                    int previousNameCost = distances[nameIndex - 1][blacklistedNameIndex] + 1;
+                    int previousBlackListedCost = distances[nameIndex][blacklistedNameIndex - 1] + 1;
+                    int previousMinimumCost = Math.min(previousNameCost, previousBlackListedCost);
+
                     int cost = name.charAt(nameIndex - 1) == blacklistedName.charAt(blacklistedNameIndex - 1) ? 0 : 1;
-                    distance[nameIndex][blacklistedNameIndex] = Math.min(
-                            Math.min(distance[nameIndex - 1][blacklistedNameIndex] + 1, distance[nameIndex][blacklistedNameIndex - 1] + 1),
-                            distance[nameIndex - 1][blacklistedNameIndex - 1] + cost);
+                    int previousWithCurrentCost = distances[nameIndex - 1][blacklistedNameIndex - 1] + cost;
+
+                    distances[nameIndex][blacklistedNameIndex] = Math.min(previousMinimumCost, previousWithCurrentCost);
                 }
             }
         }
 
-        return distance[name.length()][blacklistedName.length()];
+        return distances[name.length()][blacklistedName.length()];
     }
 }
